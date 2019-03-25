76b7a461ac33eeb0dfd17f2a78836aaf276eb747
unknown <jumariojs@gmail.com>
2019-03-25 17:04:41 -0300

    Escopo do projeto previamente criado, mais informações encontram-se no README.MD

---
+++ src/activity/readme.md
@@ -0,0 +1,28 @@
+<!-- Está atividade é uma avaliação que recebi em um periodo onde estava a procura de emprego. A atividade em si era para ser feita com React, mas como não possuo conhecimento da lib no momento fiz com JavaScript OO :D. Em breve irei estudar sobre React e postarei o mesmo repositório com as mesmas funções em React. Abaixo encontra-se o desafio original. -->
+
+## AVALIAÇÃO
+ `Objetivo Geral`:
+Desenvolver uma aplicação web (de uma página) que consuma uma API e
+mostre o resultado.
+
+`Objetivos Específicos`:
+• Consumir serviço, seguindo a documentação da API:
+https://rickandmortyapi.com/documentation/
+• Utilizar o end point: GET
+https://rickandmortyapi.com/api/character/
+• Com o resultado, mostrar as seguintes informações: id, name, status,
+species e image.
+• Monte a interface utilizando cards.
+• Exibir os cards em paginação. Max: 10 Cards por página
+• Ao clicar no card do respectivo personagem deve exibir as mesmas
+informações {id, name, status, species e image } e quais episódios este
+participou na serie.
+• Aplicar filtros de busca por personagem.
+• Utilizar React como framework Javascript.
+• Pode utilizar qualquer framework CSS – preferência seguindo o Material
+Design como guia.
+• Ao finalizar o teste responda este e-mail com o links do repositório no
+Github e da aplicação publicada no Heroku.
+
+`Obs`: Sinta-se livre para adicionar quaisquer incrementos na aplicação, os mesmos
+serão considerados um diferencial na avaliação final.
---
+++ src/controller/characterSync.js
@@ -0,0 +1,91 @@
+// Por questões didáticas abaixo estarão alguns comentários explicando o que cada função faz.
+
+class characterSync{
+    constructor(){
+        this.request = new XMLHttpRequest();
+        this.endpoint = "https://rickandmortyapi.com/api/character/";
+        this.container = document.querySelector('.container');
+        this.searchResult;
+        this.recoverCharacters(this.request,this.endpoint);
+        this.limiterCount = 0;
+    }
+    /**
+    *    Função responsável por recuperar da API todos os characters e envia-lo para construçào 
+    */ 
+    async recoverCharacters(request,endpoint) {
+        request.open('GET', endpoint,true);
+        await request.send()
+        request.onreadystatechange  = () =>{
+            if (this.request.readyState == 4){
+                /*  ****************************** NOTA *********************************
+                *   Neste caso adicionei a response também em uma variável local para que 
+                *   não fosse necessário ficar executando a ação sempre.JSON
+                */
+                this.searchResult = JSON.parse(this.request.response)
+                return this.constructCharacters(JSON.parse(this.request.response))
+            }
+        }
+    }
+    /**
+     *  Responsável pela Paginação dos personagens, desde a criação dos botões à inserção de funcionalidade 
+     * em cada um deles
+     */
+    pagination(lista, selected = 1){
+        let quantidade = lista.length / 3;
+        let pagin = "";
+        let pagination = document.createElement('div');
+        for(let i = 0; i < quantidade ; i++) {
+            pagin += `     <div class="pages">
+              <button class="button but${i}">${i + 1}</button>
+             </div>  `;
+        }
+        pagination.innerHTML = pagin;
+        this.container.appendChild(pagination);
+        document.querySelectorAll('.button').forEach(selected =>{
+            selected.addEventListener('click', event=>{
+                this.constructCharacters(this.searchResult,parseFloat(event.target.innerHTML))
+            })
+        });
+        document.querySelector(`.but${selected-1}`).className += " select";
+
+    }
+
+    /**
+     * Aqui será criada a limitação dos meus cards(onde direi que quero apenas 3 personagens por vez)
+     * e onde limitárei o resultado obtido na requisiçào feita em recoverCharacters(request,endpoint) 
+     */
+    characterLimit(list,page = 1){
+        let selectedPage = page;
+        page = ( page * 3) - 3;
+        let filteredList = list.filter((list,index) =>{
+            return index >= page && index <= page+2
+        });
+        this.pagination(list,selectedPage);
+        return filteredList;
+    }
+
+    /**
+     * Por fim mas não menos importante, aqui serão criados e adicionados todos os Personagens 
+     */
+    constructCharacters(response,page = 1){
+       let characterRows =  document.createElement('div');
+       this.container.innerHTML = "";
+       let data = this.characterLimit(response.results,page);
+           characterRows.innerHTML = data.map(result =>{
+            return `
+         <div class="space">
+             <div class="character">
+                <img class="image" src="${result.image}">
+                <span class="character_id">${result.id}</span>
+                <span class="character_name">${result.name}</span>
+                <span class="species">${result.species}</span>
+            </div>
+        </div>`
+        }).join("");
+        this.container.appendChild(characterRows)
+    }
+
+
+}
+
+
